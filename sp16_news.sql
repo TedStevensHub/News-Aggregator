5a7a6210fe61b343e63f077ac5faa6b01bb375d6
TedStevensHub <tedstevens@comcast.net>
2016-05-22 14:21:30 -0700

    Adds database schema and data

---
+++ sp16_news.sql
@@ -0,0 +1,46 @@
+/*
+sp16_news.sql
+*/
+
+SET foreign_key_checks = 0; #turn off constraints temporarily
+
+#since constraints cause problems, drop tables first, working backward
+DROP TABLE IF EXISTS sp16_newsFeed;
+  
+#all tables must be of type InnoDB to do transactions, foreign key constraints
+CREATE TABLE sp16_newsFeed(
+FeedID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CategoryID INT UNSIGNED DEFAULT 0,
+Description TEXT DEFAULT '',
+PRIMARY KEY (FeedID),
+FOREIGN KEY (CategoryID) REFERENCES sp16_newsCategory(CategoryID) ON DELETE CASCADE
+)ENGINE=INNODB; 
+
+INSERT INTO sp16_newsFeed VALUES (NULL,1,'Mount+Everest');
+INSERT INTO sp16_newsFeed VALUES (NULL,1,'Bangladesh');
+INSERT INTO sp16_newsFeed VALUES (NULL,1,'Taliban');
+INSERT INTO sp16_newsFeed VALUES (NULL,2,'NASA'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,2,'Maui'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,2,'2020+Summer+Olympics'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,3,'Boston+Red+Sox'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,3,'French+Open'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,3,'Cleveland+Cavaliers', ''); 
+
+
+#--URL's to make dynamic--#
+#https://news.google.com/news/section?cf=all&hl=en&pz=1&ned=us&q=EgyptAir&topicsid=en_us:w&ict=tnv0
+#https://news.google.com/news/section?cf=all&hl=en&pz=1&ned=us&q=Maui&topicsid=en_us:snc&ict=tnv3
+#https://news.google.com/news/section?cf=all&hl=en&pz=1&ned=us&q=French+Open&topicsid=en_us:s&ict=tnv3
+
+CREATE TABLE sp16_newsCategory(
+CategoryID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+Category VARCHAR(100) DEFAULT '',
+CategorySymbol VARCHAR(20) DEFAULT '',
+PRIMARY KEY (CategoryID),    
+)ENGINE=INNODB;
+
+INSERT INTO sp16_newsCategory VALUES (NULL,'World','w');
+INSERT INTO sp16_newsCategory VALUES (NULL,'Science','snc');
+INSERT INTO sp16_newsCategory VALUES (NULL,'Sports','s');
+
+SET foreign_key_checks = 1; #turn foreign key check back on
