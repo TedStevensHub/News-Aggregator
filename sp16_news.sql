bafc829eb4b6049455c578bd29558ad3d1ecc12a
TedStevensHub <tedstevens@comcast.net>
2016-05-24 17:25:07 -0700

    Update sp16_news.sql

--- sp16_news.sql
+++ sp16_news.sql
@@ -10,20 +10,21 @@ DROP TABLE IF EXISTS sp16_newsFeed;
 #all tables must be of type InnoDB to do transactions, foreign key constraints
 CREATE TABLE sp16_newsFeed(
 FeedID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+FeedName TEXT DEFAULT '',
 CategoryID INT UNSIGNED DEFAULT 0,
 Feed TEXT DEFAULT '',
 PRIMARY KEY (FeedID),
 FOREIGN KEY (CategoryID) REFERENCES sp16_newsCategory(CategoryID) ON DELETE CASCADE
 )ENGINE=INNODB; 
 
-INSERT INTO sp16_newsFeed VALUES (NULL,1,'Mount+Everest');
-INSERT INTO sp16_newsFeed VALUES (NULL,1,'Bangladesh');
-INSERT INTO sp16_newsFeed VALUES (NULL,1,'Taliban');
-INSERT INTO sp16_newsFeed VALUES (NULL,2,'NASA'); 
-INSERT INTO sp16_newsFeed VALUES (NULL,2,'Maui'); 
-INSERT INTO sp16_newsFeed VALUES (NULL,2,'2020+Summer+Olympics'); 
-INSERT INTO sp16_newsFeed VALUES (NULL,3,'Boston+Red+Sox'); 
-INSERT INTO sp16_newsFeed VALUES (NULL,3,'French+Open'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,'Mount Everest',1,'Mount+Everest');
+INSERT INTO sp16_newsFeed VALUES (NULL,Bangladesh,1,'Bangladesh');
+INSERT INTO sp16_newsFeed VALUES (NULL,Taliban,1,'Taliban');
+INSERT INTO sp16_newsFeed VALUES (NULL,Nasa,2,'NASA'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,Maui,2,'Maui'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,2020 Summer Olympics,2,'2020+Summer+Olympics'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,Boston Red Sox,3,'Boston+Red+Sox'); 
+INSERT INTO sp16_newsFeed VALUES (NULL,French Open,3,'French+Open'); 
 INSERT INTO sp16_newsFeed VALUES (NULL,3,'Cleveland+Cavaliers'); 
 
 
