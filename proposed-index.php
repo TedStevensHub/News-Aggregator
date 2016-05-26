37e09d3bd47e61aee5452a64093d673bd6d257e7
Sam McHaney <luomopuma@gmail.com>
2016-05-26 14:05:15 -0700

    Propose index structure
    Proposal of index.php structure, caching data to SESSION superglobal.

---
+++ proposed-index.php
@@ -0,0 +1,14 @@
+<?php
+// proposed-index.php
+// Sam McHaney
+
+// Start session
+// Open DB connection
+// Query NewsCategories
+// Cache NewsCategories to SESSION
+// Query RSS feeds
+// Parse/Cache XML to SESSION
+// Close database connection
+
+// Loop through each category element in SESSION
+//// echo links
