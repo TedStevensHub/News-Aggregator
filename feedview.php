b7c4350abf2a7d4c5daae34a50c57aa19c085fbc
ted <tedstevens@comcast.net>
2016-06-07 15:07:58 -0700

    adds css and recent code
    ted

--- feedview.php
+++ feedview.php
@@ -5,43 +5,44 @@ include 'credentials.php';
 
 session_start();
 //global session that everyone is accessing?
-
-$Feed = $_GET['feed'];
 $FeedID = $_GET['feedid'];
 
-
-$sqltime = "SELECT lastUpdated FROM sp16_newsFeed WHERE FeedID=$FeedID";
-$connect = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-$result = mysqli_query($connect,$sqltime) or die(trigger_error(mysqli_error($connect), E_USER_ERROR));
-
-//get last refresh of rss feed
-while ($row = mysqli_fetch_assoc($result)) {
-    $lastUpdated = $row['lastUpdated'];
-}   
-
-@mysqli_free_result($result);
-
-//add 10 minutes. this new date is when the rss feed can be refreshed
-//$pastTime = strtotime("$lastUpdated + 10 minutes");
-$date = date_create("$lastUpdated");
-date_add($date, date_interval_create_from_date_string('10 minutes'));
-$pastTime = date_format($date, 'Y-m-d H:i:s');
 
 //current time
 $currentTime = date("Y-m-d H:i:s");
     
+
+
+
+
+if(!isset($_SESSION['feeds'][$FeedID])){
+    //default to a time 15 minutes in the past
+    $pastTime = date_sub(new DateTime($currentTime), date_interval_create_from_date_string('15 minutes'));
+} else {
+    $session = $_SESSION["feeds"];
+    $date = new DateTime($session[$FeedID][1]);
+    date_add($date, date_interval_create_from_date_string('10 minutes'));
+    
+    $thePastTime = date_format($date, 'Y-m-d H:i:s');
+}
+
+$theTime = date("Y-m-d H:i:s");
 echo "Current time: $currentTime <br />";
-
-echo "Next available update: $pastTime <br />";
-
-
-
+echo "Next available update: $thePastTime <br />";
 
 //compare current time to time when rss feed can be refreshed
-if ($currentTime > $pastTime) {
+//display new feed
+if ($currentTime > $thePastTime) {
     //display refreshed rss feed then update session and db
-    $sqlupdate = "UPDATE sp16_newsFeed SET lastUpdated=NOW() WHERE FeedID=$FeedID";
-    $connect->query($sqlupdate);
+    $sql1 = "SELECT Feed FROM sp16_newsFeed WHERE FeedID=$FeedID";
+    $connect = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+    $result = mysqli_query($connect,$sql1) or die(trigger_error(mysqli_error($connect), E_USER_ERROR));
+    
+    while($row = mysqli_fetch_assoc($result)) {
+        $Feed = $row['Feed'];
+    }
+    @mysqli_free_result($result);
+    @mysqli_close($connect);
     
     $request = "https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=".$Feed."&output=rss";
     $response = file_get_contents($request);
@@ -57,11 +58,11 @@ if ($currentTime > $pastTime) {
     
     //update session
     $f[$FeedID][0]=$response;
+    $f[$FeedID][1]=$currentTime;
     $_SESSION["feeds"]=$f;
     
 } else {
     //display old cached feed
-    $session = $_SESSION["feeds"];
     $string = $session[$FeedID][0];
     $xml = simplexml_load_string($string);
     
@@ -72,9 +73,9 @@ if ($currentTime > $pastTime) {
         echo '<p>' . $story->description . '</p><br /><br />';
     }
 
-    
 }
 
-@mysqli_close($connect);
+
+
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<link href="news.css" rel="stylesheet" type="text/css" media="all">
+
 <?php
 //index2.php
 
@@ -5,7 +7,7 @@ include 'credentials.php';
 
 
 //SQL statement selects Cateogries that exist in both tables
-$sql = "SELECT Category FROM sp16_newsCategory";
+$sql = "SELECT DISTINCT Category FROM sp16_newsCategory nc INNER JOIN sp16_newsFeed nf ON nc.CategoryID = nf.CategoryID";
 
 
 $connect = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
@@ -15,37 +17,36 @@ $result = mysqli_query($connect,$sql) or die(trigger_error(mysqli_error($connect
 
 //an array of objects, an object is 1 of 9 of the feeds, each object has all the information from the database portaining to it. They are added to 1 of 3 arrays for each cateogry
 
-echo '<h3 align="center">News Aggregator</h3>';
+echo '<h1 align="center">News Aggregator</h1>';
 
 while ($row = mysqli_fetch_assoc($result)) {
     $category = $row['Category'];
     echo "<br/><b>$category</b><br/>";
-    $sql2 = "SELECT FeedName, Feed, FeedID FROM sp16_newsFeed nf INNER JOIN sp16_newsCategory nc ON nf.CategoryID = nc.CategoryID WHERE Category = '$category'";
+    $sql2 = "SELECT FeedName, FeedID FROM sp16_newsFeed nf INNER JOIN sp16_newsCategory nc ON nc.CategoryID = nf.CategoryID WHERE Category = '$category'";
     $result2 = mysqli_query($connect,$sql2) or die(trigger_error(mysqli_error($connect), E_USER_ERROR));    
     while ($row2 = mysqli_fetch_assoc($result2)) {
-        $feedlink = new Feed($row2['FeedName'], $row2['Feed'], $row2['FeedID']);
+        $feedlink = new Feed($row2['FeedName'], $row2['FeedID']);
         $feedlink->FeedLinks();      
     }   
 }
 @mysqli_free_result($result);
+@mysqli_free_result($result2);
 @mysqli_close($connect);
 
 
 class Feed {
     public $FeedName = '';
-    public $Feed = '';
     public $FeedID = 0;
     
-    function __construct($FeedName, $Feed, $FeedID) {
+    function __construct($FeedName, $FeedID) {
         
         $this->FeedName = $FeedName;
-        $this->Feed = $Feed;
         $this->FeedID = $FeedID;
 
     }//end constructor
     
     public function FeedLinks() {
-    echo '<a href="feedview.php?feed='.urlencode($this->Feed).'&feedid='.$this->FeedID.'">'.$this->FeedName.'</a><br/>';
+    echo '<a href="feedview.php?feedid='.$this->FeedID.'">'.$this->FeedName.'</a><br/>';
     }
     
 }//end class
